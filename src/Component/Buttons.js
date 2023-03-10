6a6eacf04b860f20bd4fada0d130e9771e43f135
JasonDeee <86407960+JasonDeee@users.noreply.github.com>
2023-03-10 08:55:35 +0700

    Slider Button First Commit

--- src/Component/Buttons.js
+++ src/Component/Buttons.js
@@ -3,6 +3,7 @@ import "../Styles/App.css";
 const ButtonFull = (Props) => {
   return (
     <button className="ButtonContainer Button_Full">
+      <div className="Button_Overlay"></div>
       <div
         className="Button_Icon"
         style={{ backgroundImage: `url("${Props.IconURL}")` }}
@@ -12,4 +13,14 @@ const ButtonFull = (Props) => {
   );
 };
 
-export { ButtonFull };
+const ButtonSlider = (Props) => {
+  return (
+    <button className="ButtonContainer Button_Slider">
+      <div className="Button_Overlay"></div>
+      <div className="Button_Arrow" id="Prev"></div>
+      <div className="Button_Arrow" id="Next"></div>
+    </button>
+  );
+};
+
+export { ButtonFull, ButtonSlider };
--- src/Component/Homepage.js
+++ src/Component/Homepage.js
@@ -1,6 +1,6 @@
 import "../Styles/App.css";
 import { useEffect, useState } from "react";
-import { ButtonFull } from "./Buttons";
+import { ButtonFull, ButtonSlider } from "./Buttons";
 
 import Copy_Logo from "../Assets/Copy_Logo.svg";
 
@@ -10,7 +10,9 @@ const HomepageFloat = () => {
       <section className="Section" id="Sec1Float">
         <ButtonFull IconURL={Copy_Logo} Label="Khám phá"></ButtonFull>
       </section>
-      <section className="Section" id="Sec2Float"></section>{" "}
+      <section className="Section" id="Sec2Float">
+        <ButtonSlider></ButtonSlider>
+      </section>{" "}
       <section className="Section" id="Sec3Float"></section>{" "}
       <section className="Section" id="Sec5Float"></section>
     </main>
